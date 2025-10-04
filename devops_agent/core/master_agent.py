edf8fc6dcfac88ac5902bc65d3a386c8838ea5e5
manthapavankumar11@gmail.com <manthapavankumar11@gmail.com>
2025-10-04 19:38:11 +0530

    fully working code for devops agent with multiple agent strategy

---
+++ devops_agent/core/master_agent.py
@@ -0,0 +1,74 @@
+import asyncio
+import os
+
+from agno.knowledge import Knowledge
+from agno.models.openai import OpenAIChat
+from agno.models.anthropic import Claude
+from agno.models.google.gemini import Gemini
+from agno.team import Team
+from agno.vectordb.qdrant import Qdrant
+from agno.knowledge.embedder.fastembed import FastEmbedEmbedder
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import VectorParams, Distance
+
+from devops_agent.core.devops_agent import execute_devops_agent
+from devops_agent.core.kubernetes_agent import execute_k8s_agent
+
+qclient = QdrantClient(url=os.environ.get('QDRANT_URL'), api_key=os.environ.get('QDRANT_API_KEY'))
+if not qclient.collection_exists("devops-memory"):
+    qclient.create_collection(collection_name="devops-memory",
+                              vectors_config=VectorParams(size=768, distance=Distance.COSINE))
+
+vector_db = Qdrant(collection="devops-memory", url=os.environ.get('QDRANT_URL'),
+                   api_key=os.environ.get('QDRANT_API_KEY'),
+                   embedder=FastEmbedEmbedder(id="snowflake/snowflake-arctic-embed-m"))
+
+# Create knowledge base
+knowledge = Knowledge(vector_db=vector_db)
+
+
+def execute_master_agent(provider: str, user_query: str) -> str:
+    llm_provider = provider.lower().strip()
+    if llm_provider == 'openai':
+        model = OpenAIChat(id="gpt-5-mini", api_key=os.environ.get('OPENAI_API_KEY'))
+    elif llm_provider == 'anthropic':
+        model = Claude(id="claude-sonnet-4-5-20250929", temperature=0.6, api_key=os.environ.get('ANTHROPIC_API_KEY'))
+    elif llm_provider == 'google':
+        model = Gemini(id="gemini-2.5-flash", temperature=0.6, api_key=os.environ.get('GEMINI_API_KEY'))
+    else:
+        model = OpenAIChat(id="gpt-5-mini"),  # default
+
+    devops_team = Team(
+        name="Multi Cloud and Devops Team",
+        model=model,
+        respond_directly=True,
+        members=[
+            execute_devops_agent(provider=provider),
+            execute_k8s_agent(provider=provider),
+        ],
+        markdown=True,
+        instructions=[
+            "You are a intelligent router that directs questions to the appropriate agent.",
+            "If the user asks in a non devops or k8s question whose agent is not a team member, respond in English with:",
+            "'I can only answer in the following technologies: Devops & Kubernetes Architecture on Multiple clouds. Please ask your question in one of these technologies.'",
+            "Always check the technology or domain of the user's input before routing to an agent.",
+            "For unsupported technologies like coding, flowcharts, analytics etc respond in English with the above message.",
+        ],
+        knowledge=knowledge,
+        determine_input_for_members=True,
+        stream_intermediate_steps=True,
+        add_knowledge_to_context=True,
+        add_datetime_to_context=True,
+        add_session_summary_to_context=True,
+        show_members_responses=True,
+    )
+
+    response = devops_team.run(user_query, stream_intermediate_steps=True, retry=3)
+
+    # saved the response to knowledge in async mode
+    asyncio.run(
+        knowledge.add_content_async(text_content=f"question: {user_query}, Assistant: {response.content}",
+                                    metadata={"agent_id": response.team_id, "session_id": response.session_id})
+    )
+
+    return response.content
