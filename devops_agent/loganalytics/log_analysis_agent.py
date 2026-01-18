9a5d83691c41fbe82e588523d58fbf5e1e010761
manthapavankumar11@gmail.com <manthapavankumar11@gmail.com>
2026-01-18 22:27:51 +0530

    adding MCP for analysis of logs

--- devops_agent/loganalytics/log_analysis_agent.py
+++ devops_agent/loganalytics/log_analysis_agent.py
@@ -3,14 +3,16 @@ from pathlib import Path
 
 from agno.agent import Agent
 from agno.media import File
+from agno.tools.mcp import MultiMCPTools
+
 from devops_agent.utils.model_provider import get_model
 from rich.console import Console
 from rich.panel import Panel
 
 console = Console()
 
-def execute_log_analysis_agent(provider: str, model: str, log_file: Path, debug_mode: bool= False,
-                               reasoning:bool=False) -> Agent:
+async def execute_log_analysis_agent(provider: str, model: str, log_file: Path, debug_mode: bool= False,
+                               reasoning:bool=False, filesystem_paths:str=None) -> Agent:
     console.print(Panel.fit(
         "[bold cyan]Log Analysis Agent Invoking...[/bold cyan]",
         border_style="cyan"
@@ -18,9 +20,19 @@ def execute_log_analysis_agent(provider: str, model: str, log_file: Path, debug_
 
     model = get_model(provider=provider, model_str=model)
 
+    # Initialize and connect to multiple MCP servers
+    mcp_tools = MultiMCPTools(
+        commands=[
+            f"npx -y @modelcontextprotocol/server-filesystem {filesystem_paths}",
+        ],
+    )
+
+    await mcp_tools.connect()
+
     file_analysis_agent = Agent(
         name="LogFile Analysis Agent",
         role="Analyze log files",
+        tools=[mcp_tools],
         model=model,
         description="You are an AI agent that can analyze log files.",
         instructions=[
